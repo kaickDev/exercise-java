e1223fcf8f6853ae602702495d4eca6029c1141b
kaickDev <kaickdevbr@gmail.com>
2025-10-23 17:38:49 -0300

    Update Main.java
    Added input handling to check if a number is positive, negative, or zero.

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("enter a number: ");
+        int num = input.nextInt();
+
+        if (num < 0){
+           System.out.println("your number is negative");
+        }
+        else if (num > 0){
+
+            System.out.println("your number is positive");
+            if (num == 0){
+
+                System.out.println("your number is zero");
+
+            }
+        }
+    }
+}
